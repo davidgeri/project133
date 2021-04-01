5a205bc2d7dbe59cd954d799a12a5f676b50efbd
David Geri <davidgeri988@gmail.com>
2021-04-01 20:32:45 +0800

    porfolio

---
+++ resources/views/porfolio.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+	<title>Resume</title>
+	<link rel="icon" href="/img/avengers.png">
+	<style type="text/css">
+		body {
+			background-image: linear-gradient(to right, #F0E68C, #6db8ea);
+		}
+	</style>
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light shadow" style="background-image: linear-gradient(to right, #F0E68C, #6db8ea);">
+		<a class="navbar-brand" href="#">About me</a>
+		<div class="navbar-nav ml-auto">
+			<div class="icon">
+				<a class="tombol mr-3" href="https://www.facebook.com/DavidakaMaJesTY" target="_blank"><i class="fab
+						fa-facebook-f"></i></a>
+				<a class="tombol mr-3" href="https://www.instagram.com/davidgerii/" target="_blank"><i class="fab
+						fa-instagram"></i></a>
+			</div>
+		</div>
+		</div>
+	</nav>
+	<div class="jumbotron jumbotron-fluid" style="background-image: linear-gradient(to right, #F0E68C, #6db8ea);">
+	</div>
+
+	<div class="container" style="margin-top: -80px">
+		<div class="card shadow mb-5">
+			<div class="card-body">
+				<div class="row align-items-center mb-3">
+					<div class="col-2">
+						<img src="/img/foto.jpeg" class="img-fluid rounded-circle">
+					</div>
+					<div class="col-8">
+						<h4>
+							Let me tell you about my self!
+						</h4>
+						<p>
+							David Geri,
+							Seseorang yang memiliki ketertarikan yang sangat tinggi dengan makanan.<br>
+						</p>
+					</div>
+
+
+					<!-- Modal -->
+					<div class="modal fade" id="modal">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLabel">Message From David</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<blockquote class="blockquote text-center">
+										<p class="mb-0">"Hi everyone! Nice to meet you!"</p>
+									</blockquote>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr>
+			<h4 class="text-center mb-4">Introduction</h4>
+			<div class="row align-items-center">
+				<div class="col-8">
+					<table class="table table-bordered table-sm">
+						<tbody>
+							<tr>
+								<td>Nama Lengkap</td>
+								<td class="text-center">David Geri Pradipta Riadi</td>
+							</tr>
+							<tr>
+								<td>Tempat/Tanggal Lahir</td>
+								<td class="text-center">Mataram, 27 April 2001</td>
+							</tr>
+							<tr>
+								<td>Usia</td>
+								<td class="text-center">19 Tahun</td>
+							</tr>
+							<tr>
+								<td>Status</td>
+								<td class="text-center">Mahasiswa</td>
+							</tr>
+							<tr>
+								<td>Universitas</td>
+								<td class="text-center">Universitas Bumigora</td>
+							</tr>
+							<tr>
+								<td>NIM</td>
+								<td class="text-center">1810530133</td>
+							</tr>
+							<tr>
+								<td>Hobi</td>
+								<td class="text-center">Makan, Tidur, Gaming</td>
+							</tr>
+
+						</tbody>
+					</table>
+				</div>
+				<div class="col-4">
+					<p class="lead text-center font-italic">
+						"If you wanna chase your dream,<br>
+						the first thing you gotta do is wake up"
+					</p>
+				</div>
+			</div>
+			<div class="row align-items-center">
+				<div class="col-6">
+					<h4 class="text-left mb-4 mt-5 pt-2">Pendidikan</h4>
+					<table class="table table-striped table-sm">
+						<tbody>
+							<tr>
+								<td class="text-center">2012-2015</td>
+								<td>SMP Kristen Aletheia Ampenan</td>
+							</tr>
+							<tr>
+								<td class="text-center">2015-2018</td>
+								<td>SMA Negeri 8 Mataram</td>
+							</tr>
+							<tr>
+								<td class="text-center">2018-Sekarang</td>
+								<td>Universitas Bumigora Mataram</td>
+							</tr>
+
+						</tbody>
+					</table>
+				</div>
+				<div class="col-6">
+					<h4 class="text-left mb-4 pt-2">Achievement</h4>
+					<table class="table table-bordered table-sm">
+						<tbody>
+							<tr>
+								<td class="text-center">1</td>
+								<td>Runner Up DBL(Developmental Basketball Junior) Mataram 2014</td>
+							</tr>
+							<tr>
+								<td class="text-center">2</td>
+								<td>Patricipated in Unram English Debate Competition (2017)</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="./assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
